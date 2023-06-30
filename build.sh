e09ef30c241e77861294b77fd7ccbc8a611966ac
cseiberling <89859574+cseiberling@users.noreply.github.com>
2023-06-29 19:29:16 -0500

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+BRANCH=main
+sed -i "s/BRANCHVERSION/$BRANCH/g" pom.xml
+
+#PRSCANSPACE="Default"
+# For Pull requests:
+PRSCANSPACE="https://github.com/cseiberling/JavaClientDemo|feature/enhancement1"
+sed -i "s/SCANSPACE/$SCANSPACE/g" settings-override.xml
+
+#mvn clean package -DskipTests
+mvn -X -U clean package com.codelogic.agent.java:jcape-maven-plugin:23.11.17:codelogic-scan-csv -am -e -U -s settings-override.xml -DskipTests
