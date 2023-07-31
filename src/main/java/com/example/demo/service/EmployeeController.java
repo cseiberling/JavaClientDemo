5ce046905ef1613c7b06371350313f2e3882300d
cseiberling <89859574+cseiberling@users.noreply.github.com>
2023-07-31 16:56:41 -0500

    Create EmployeeController.java

---
+++ src/main/java/com/example/demo/service/EmployeeController.java
@@ -0,0 +1,39 @@
+package com.example.storedproc.domain;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+
+@Data
+@Entity
+@NoArgsConstructor
+@Builder
+@AllArgsConstructor
+@NamedStoredProcedureQuery(
+        name = "addEmployeeThroughNamedStoredProcedureQuery",
+        procedureName = "EMPLOYEEPROCEDURE",
+        parameters = {
+                @StoredProcedureParameter(name = "FIRST_NAME", mode = ParameterMode.IN, type = String.class),
+                @StoredProcedureParameter(name = "LAST_NAME", mode = ParameterMode.IN, type = String.class),
+                @StoredProcedureParameter(name = "EMAIL", mode = ParameterMode.INOUT, type = String.class),
+                @StoredProcedureParameter(name = "ID", mode = ParameterMode.OUT, type = Integer.class),
+                @StoredProcedureParameter(name = "CREATED_AT", mode = ParameterMode.OUT, type = Date.class),
+        }
+)
+public class Employee implements Serializable {
+    @Id
+    private Integer id;
+
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd@HH:mm:ss.SSSZ")
+    private Date createdAt;
+}
